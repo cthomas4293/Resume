64a299776417fb8f6fe189d46a5d16a5efc6ec1a
Carl Thomas <cthomas4293@hotmail.com>
2021-05-29 13:30:40 -0400

    added javascript scroll event

--- bootstrap example/script.js
+++ bootstrap example/script.js
@@ -9,10 +9,20 @@
 //   }
 // });
 
+const navbar = document.querySelector(".main-navbar");
+const stickyNavbar = document.querySelector("#navbar");
+
+const initialCoords = navbar.getBoundingClientRect();
+
+console.log(initialCoords);
+
 window.addEventListener("scroll", function () {
   console.log(window.scrollY);
 
-  if (window.scrollY === 200) {
-    document.querySelector("#navbar").setAttribute("style", "top: 0;");
+  if (window.scrollY >= initialCoords.bottom) {
+    stickyNavbar.setAttribute("style", "top: 0;");
+  } else {
+    stickyNavbar.setAttribute("style", "top: -150;");
+    stickyNavbar.setAttribute("style", "transition-duration: 0;");
   }
 });

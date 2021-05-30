92bc3500d75671aefc2f3c96623a46474f1ea8ef
Carl Thomas <cthomas4293@hotmail.com>
2021-05-29 20:42:51 -0400

    added an observer for the sticky navbar

--- bootstrap example/script.js
+++ bootstrap example/script.js
@@ -12,17 +12,38 @@
 const navbar = document.querySelector(".main-navbar");
 const stickyNavbar = document.querySelector("#navbar");
 
-const initialCoords = navbar.getBoundingClientRect();
+// const initialCoords = navbar.getBoundingClientRect();
 
-console.log(initialCoords);
+// console.log(initialCoords);
 
-window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
+// window.addEventListener("scroll", function () {
+//   console.log(window.scrollY);
 
-  if (window.scrollY >= initialCoords.bottom) {
-    stickyNavbar.setAttribute("style", "top: 0;");
-  } else {
-    stickyNavbar.setAttribute("style", "top: -150;");
-    stickyNavbar.setAttribute("style", "transition-duration: 0;");
-  }
-});
+//   if (window.scrollY >= initialCoords.bottom) {
+//     stickyNavbar.setAttribute("style", "top: 0;");
+//   } else {
+//     stickyNavbar.setAttribute("style", "top: -150;");
+//     stickyNavbar.setAttribute("style", "transition-duration: 0;");
+//   }
+// });
+
+// Intersection Observer API
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting == false) {
+      stickyNavbar.setAttribute("style", "top: 0;");
+    } else {
+      stickyNavbar.setAttribute("style", "top: -150;");
+      stickyNavbar.setAttribute("style", "transition-duration: 0;");
+    }
+  });
+};
+
+const obsOptions = {
+  root: null, // Viewport is threshold
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+observer.observe(navbar);
